481742d3f025afe47cef08cbc4d6d5d7b6349a17
Mia-Salazar <maria.sgr@gmail.com>
2020-07-25 18:47:33 +0200

    Refactor of footer and erase unnecessary components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Header } from './components/header/header.component'
-import { Body } from './components/body/body.component';
-import { FooterComponent } from './components/footer/footer.component'
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './components/home/home.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     Header,
-    Body,
-    FooterComponent
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-footer',
+  selector: 'footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+export class FooterComponent {
+  show:boolean = false
+  me = {
+    author:'Mia Salazar',
+    description: 'Front end developer junior',
+    url: 'www.miasalazar.com'
   }
+  technologies:string[] = ['Html', 'Css', 'Javascript', 'Angular']
 
 }
