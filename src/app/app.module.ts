38a18a8213be9ec961e07843ac86d9a66c7cc4bd
Mia-Salazar <maria.sgr@gmail.com>
2020-07-25 20:13:19 +0200

    Add routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { APP_ROUTING } from './app.routes'
+
+//Components
 import { AppComponent } from './app.component';
 import { Header } from './components/header/header.component'
 import { FooterComponent } from './components/footer/footer.component';
-import { HomeComponent } from './components/home/home.component'
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { HeroesComponent } from './heroes/heroes.component'
 
 @NgModule({
   declarations: [
     AppComponent,
     Header,
     FooterComponent,
-    HomeComponent
+    HomeComponent,
+    AboutComponent,
+    HeroesComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
